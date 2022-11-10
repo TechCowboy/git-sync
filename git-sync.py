c496ef5983005c043c0860440a4294af797ea2c7
Norman Davie <ecompservices@gmail.com>
2022-11-10 19:47:14 -0400

    Happy with this version.  NOTE: debug flag is set

--- git-sync.py
+++ git-sync.py
@@ -5,6 +5,7 @@ import subprocess
 
 debug = 1
 
+print("Checking to see if we have out of day repositories...")
 cwd = os.getcwd()
 
 if debug:
@@ -19,18 +20,23 @@ for dir in all_dirs:
         if os.path.exists(os.path.join(dir, ".git")):
             git_dirs.append(os.path.abspath(dir))
 
-
-print("Getting latest status from remote sites ")
 total_dirs = len(git_dirs)
+print(f"{total_dirs} git directories found")
+print("Updating the latest status on the remote sites")
 for dir in git_dirs:
     os.chdir(dir)
     count = f"{total_dirs}"
-    print(f"{count} remaining    ")
+    print(f"{count:3} {dir:70}\r", end='')
     result = subprocess.run(['git', 'remote','-v','update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = result.stdout.decode('utf-8')
     total_dirs -= 1
-print()
+print(f"{' ':75}\r")
+
 pull_info = ''
+total_up_to_date=0
+
+latest_but_modified = 'Lastest from remote but local files modified'
+
 for dir in git_dirs:
     os.chdir(dir)
     result = subprocess.run(['git', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -44,21 +50,23 @@ for dir in git_dirs:
     if uptodate:
         if modified:
             but_modified = "but modified"
+            print(f"{latest_but_modified:26} {dir}")
         else:
             but_modified = ""
-        print(f"Up to date {but_modified:15} {dir}")
+            total_up_to_date += 1
+        
     else:
         
-        if behind:
-            
+        if behind:           
             if modified:
-                print(f"Files modified, can't pull {dir}")
+                print(f"{'Behind but files modified':26} {dir}")
             else:
-                print(f"Pulling {dir}")
+                print(f"{'Pulling lastest files':26} {dir}")
                 result = subprocess.run(['git', 'pull'], stdout=subprocess.PIPE)
                 pull_info += result.stdout.decode('utf-8')
 
-        
+
+print(f"{total_up_to_date} repositories are up to date")        
         
     
 """
